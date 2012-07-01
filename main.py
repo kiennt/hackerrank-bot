0cbb87b1839729d84025ef9c796353de11756deb
kiennt <trungkien2288@gmail.com>
2012-07-02 02:39:34 +0700

    [UPDATE] Run code parallel

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 
 from hackerrank import HackerRankAPI
+import sys
+import os
 
 USERNAME = "YOUR USERNAME"
 PASSWORD = "YOUR PASSWORD"
 
 if __name__ == "__main__":
+    print sys.argv[1], sys.argv[2]
+    increment = None
+    if len(sys.argv) >= 4:
+        increment = int(sys.argv[3]) 
+    if not increment: increment = 1
     bot = HackerRankAPI(USERNAME, PASSWORD)
-    bot.auto_play()
+    bot.login()
+    bot.auto_play(int(sys.argv[1]), int(sys.argv[2]), increment)
